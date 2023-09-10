177d644e900a1cb9441f51dee4f757fbabbba6c1
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-10 20:09:21 +0200

    Duplication in pom and added Tour constructor.

--- backend/src/main/java/com/example/softwareg14/entity/Tour.java
+++ backend/src/main/java/com/example/softwareg14/entity/Tour.java
@@ -19,6 +19,22 @@ public class Tour {
     private List<User> usersAttending = new ArrayList<>();
     private int maxCapacity;
 
+    public Tour(int id, String name, String description, int durationHours, int price, String image, String location, String category, Organization organization, int maxCapacity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.durationHours = durationHours;
+        this.price = price;
+        this.image = image;
+        this.location = location;
+        this.category = category;
+        this.organization = organization;
+        this.maxCapacity = maxCapacity;
+    }
+    public Tour() {
+
+    }
+
     public void addAttendant(User user) {
         if(usersAttending.size() >= maxCapacity && maxCapacity != 0) {
             throw new RuntimeException("Tour is full");
