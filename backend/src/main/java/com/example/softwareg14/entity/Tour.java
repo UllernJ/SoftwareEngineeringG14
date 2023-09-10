28ef0c80b7a4d47bd664d6203eb47a4ac48dc889
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-10 14:15:00 +0200

    Added functionality to Tour and tests.

--- backend/src/main/java/com/example/softwareg14/entity/Tour.java
+++ backend/src/main/java/com/example/softwareg14/entity/Tour.java
@@ -2,6 +2,7 @@ package com.example.softwareg14.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,5 +16,17 @@ public class Tour {
     private String location;
     private String category;
     private Organization organization;
-    private List<User> users;
+    private List<User> usersAttending = new ArrayList<>();
+    private int maxCapacity;
+
+    public void addAttendant(User user) {
+        if(usersAttending.size() >= maxCapacity && maxCapacity != 0) {
+            throw new RuntimeException("Tour is full");
+        } else if(usersAttending.contains(user)) {
+            throw new RuntimeException("User is already attending");
+        } else {
+            usersAttending.add(user);
+        }
+    }
+
 }
