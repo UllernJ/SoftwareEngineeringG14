74dd870b4c357b369290cb808f7bbda11841318b
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-12 23:12:35 +0200

    Backend: Added potential endpoints for future use.

--- backend/src/main/java/com/example/softwareg14/map/Endpoint.java
+++ backend/src/main/java/com/example/softwareg14/map/Endpoint.java
@@ -4,11 +4,14 @@ public class Endpoint {
     private static final String apiPath = "/api/";
     public static final String USER_REGISTER =  apiPath + "user/register";
     public static final String USER_LOGIN = apiPath + "user/login";
+    public static final String USER_BY_ID = apiPath + "user/{id}";
     public static final String ORGANIZATION_REGISTER = apiPath +  "organization/register";
+    public static final String ORGANIZATION_LOGIN = apiPath + "organization/login";
     public static final String ORGANIZATIONS_ALL = apiPath + "organizations/all";
     public static final String ORGANIZATION_BY_ID = apiPath + "organization/{id}";
     public static final String TOURS_ALL = apiPath + "tours/all";
     public static final String TOUR_BY_ID = apiPath + "tour/{id}";
     public static final String TOURS_BY_ORGANIZATION_ID = apiPath + "tours/organization/{id}";
     public static final String TOURS_BY_USER_ID = apiPath + "tours/user/{id}";
+
 }
