a10ee326d1b72beeb838771616ec0af778221009
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-12 23:14:05 +0200

    Backend: Added more paths

--- backend/src/main/java/com/example/softwareg14/map/Endpoint.java
+++ backend/src/main/java/com/example/softwareg14/map/Endpoint.java
@@ -10,6 +10,8 @@ public class Endpoint {
     public static final String ORGANIZATIONS_ALL = apiPath + "organizations/all";
     public static final String ORGANIZATION_BY_ID = apiPath + "organization/{id}";
     public static final String TOURS_ALL = apiPath + "tours/all";
+    public static final String TOUR_REGISTER = apiPath + "tour/register";
+    public static final String TOUR_UNREGISTER = apiPath + "tour/unregister";
     public static final String TOUR_BY_ID = apiPath + "tour/{id}";
     public static final String TOURS_BY_ORGANIZATION_ID = apiPath + "tours/organization/{id}";
     public static final String TOURS_BY_USER_ID = apiPath + "tours/user/{id}";
