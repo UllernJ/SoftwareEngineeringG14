93dc9ebb8ce41fd1af15d0d9d4c1546436e48cd6
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-13 20:13:37 +0200

    Backend: Endpoint typo.

--- backend/src/main/java/com/example/softwareg14/map/Endpoint.java
+++ backend/src/main/java/com/example/softwareg14/map/Endpoint.java
@@ -13,7 +13,7 @@ public class Endpoint {
     public static final String ORGANIZATION_BY_ID = apiPath + "organization/{id}";
     public static final String ORGANIZATION_DELETE = apiPath + "organization/delete/{id}";
     public static final String ORGANIZATION_UPDATE = apiPath + "organization/update";
-    public static final String TOURS_ALL = apiPath + "tour/all";
+    public static final String TOURS_ALL = apiPath + "tours/all";
     private static final String CREATE_TOUR = apiPath + "tour/create";
     public static final String TOUR_REGISTER_ATTENDEE = apiPath + "tour/register";
     public static final String TOUR_UNREGISTER_ATTENDEE = apiPath + "tour/unregister";
